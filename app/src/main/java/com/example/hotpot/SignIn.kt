ecbcbc7c5e8125ea2dd37713e076a22a90910e49
Long <long.nguyen@stud.h-da.de>
2023-11-30 16:03:17 +0100

    update: SignIn and SignUp checks Input before progressing

--- app/src/main/java/com/example/hotpot/SignIn.kt
+++ app/src/main/java/com/example/hotpot/SignIn.kt
@@ -1,12 +1,18 @@
 package com.example.hotpot
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.EditText
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,6 +38,7 @@ class SignIn : Fragment() {
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,15 +46,67 @@ class SignIn : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
 
-        val signUpBtn = view.findViewById<Button>(R.id.login_signUp_tab)
+        val signUpTab = view.findViewById<Button>(R.id.login_signUp_tab)
 
-        signUpBtn.setOnClickListener {
-            signUpBtn.setOnClickListener {
-                (activity as? LoginActivity)?.showSignUpFragment()
-            }
+        val editEmail = view.findViewById<EditText>(R.id.signInEmailAddress)
+        val editPassword = view.findViewById<EditText>(R.id.signInPassword);
 
+        var correctEmail : Boolean = false;
+        var correctPassword : Boolean = false;
+
+        signUpTab.setOnClickListener {
+            // Navigate to the SignUp fragment
+            (activity as? LoginActivity)?.showSignUpFragment()
         }
 
+        val signInBtn = view.findViewById<Button>(R.id.signIn_signIn_Btn);
+
+        signInBtn.setOnClickListener {
+            if(correctEmail && correctPassword) {
+                // change activity to main screen
+                val toast = Toast.makeText(requireContext(), "Main Screen", Toast.LENGTH_SHORT)
+                toast.show()
+            } else {
+                val toast = Toast.makeText(requireContext(), "Invalid input. Try again", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+
+        editEmail.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                // Wenn editName nicht mehr ausgewählt ist, überprüfe den Text und zeige das Checkmark an, wenn nötig
+                correctEmail = editEmail.text.isNotEmpty()
+            }
+        }
+        editPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                // Wenn editName nicht mehr ausgewählt ist, überprüfe den Text und zeige das Checkmark an, wenn nötig
+                correctPassword = editPassword.text.isNotEmpty()
+            }
+        }
+
+
+        /**
+         * if touched outside of the editTexts
+         * set the editTexts not being focused anymore
+         * this triggers onFocusChange
+         */
+        view.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                // Überprüfe, ob der Klick außerhalb der EditTexts war
+                if (editEmail.isFocused || editPassword.isFocused) {
+                    val outRect = android.graphics.Rect()
+                    editEmail.getGlobalVisibleRect(outRect)
+                    editPassword.getGlobalVisibleRect(outRect)
+                    if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                        // Der Klick war außerhalb der EditTexts, entferne den Fokus
+                        editEmail.clearFocus()
+                        editPassword.clearFocus()
+                    }
+                }
+            }
+            false
+        }
         return view;
     }
 }
--- app/src/main/java/com/example/hotpot/SignUp.kt
+++ app/src/main/java/com/example/hotpot/SignUp.kt
@@ -1,15 +1,17 @@
 package com.example.hotpot
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -43,6 +45,7 @@ class SignUp : Fragment() {
         correctPassword = false
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -52,11 +55,9 @@ class SignUp : Fragment() {
 
         val signInTab = view.findViewById<Button>(R.id.register_signIn_tab);
 
-
-
-        val editName : EditText = view.findViewById(R.id.editTextTextName);
-        val editEmail: EditText = view.findViewById(R.id.editTextTextEmailAddress);
-        val editPassword: EditText = view.findViewById(R.id.editTextTextPassword);
+        val editName : EditText = view.findViewById(R.id.signUpName);
+        val editEmail: EditText = view.findViewById(R.id.signUpEmail);
+        val editPassword: EditText = view.findViewById(R.id.signUpPassword);
 
         // setting checkmarks invisible
         editName.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
@@ -65,28 +66,149 @@ class SignUp : Fragment() {
 
 
         signInTab.setOnClickListener {
-            checkEditTexts(editName, editEmail, editPassword)
                     (activity as? LoginActivity)?.showSignInFragment()
         }
 
         var signUpBtn = view.findViewById<Button>(R.id.signUp_signUp_btn);
         signUpBtn.setOnClickListener() {
             // check if everything is filled and maybe valid
-            checkEditTexts(editName, editEmail, editPassword)
-            // either switch to sign in or error message
-                //(activity as? LoginActivity)?.showSignInFragment()
-                Toast.makeText(requireContext(), "Invalid input. Try again", Toast.LENGTH_SHORT).show();
+            if(correctName && correctEmail && correctPassword) {
+                (activity as? LoginActivity)?.showSignInFragment()
+            } else {
+                // error message
+                //Toast.makeText(requireContext(), "Invalid input. Try again", Toast.LENGTH_SHORT).show();
+                val toast = Toast.makeText(requireContext(), "Invalid input. Try again", Toast.LENGTH_SHORT)
+                toast.show()
+            }
         }
 
-        return view;
-    }
+        /**
+         * after text is changed, checks if it's empty
+         */
 
-    private fun checkEditTexts(editName : EditText, editMail : EditText, editPassword : EditText ) {
-        if(editName.text != null)
-            correctName = true
-        if(editMail.text != null)
-            correctEmail = true;
-        if(editPassword.text != null)
-            correctPassword = true;
+        editName.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that the characters within `s` are about to be replaced with new text with a length of `count`.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that somewhere within `s`, the characters within `start` and `start + before` have been replaced with new text with a length of `count`.
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Überprüfe, ob Text vorhanden und editName nicht ausgewählt ist
+                if (!editName.isFocused && !s.isNullOrBlank()) {
+                    editName.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        0, 0, R.drawable.checkmark, 0
+                    )
+                } else {
+                    // Falls der Text leer ist oder editName ausgewählt ist, setze das Drawable auf null
+                    editName.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, 0)
+                }
+            }
+        })
+
+        editEmail.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that the characters within `s` are about to be replaced with new text with a length of `count`.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that somewhere within `s`, the characters within `start` and `start + before` have been replaced with new text with a length of `count`.
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Überprüfe, ob Text vorhanden und editEmail nicht ausgewählt ist
+                if (!editEmail.isFocused && !s.isNullOrBlank()) {
+                    editEmail.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        0, 0, R.drawable.checkmark, 0
+                    )
+                } else {
+                    // Falls der Text leer ist oder editEmail ausgewählt ist, setze das Drawable auf null
+                    editEmail.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, 0)
+                }
+            }
+        })
+
+        editPassword.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that the characters within `s` are about to be replaced with new text with a length of `count`.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called to notify you that somewhere within `s`, the characters within `start` and `start + before` have been replaced with new text with a length of `count`.
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Überprüfe, ob Text vorhanden und editPassword nicht ausgewählt ist
+                if (!editPassword.isFocused && !s.isNullOrBlank()) {
+                    editPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        0, 0, R.drawable.checkmark, 0
+                    )
+                } else {
+                    // Falls der Text leer ist oder editPassword ausgewählt ist, setze das Drawable auf null
+                    editPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, 0)
+                }
+            }
+        })
+
+        /**
+         * setFocusChangeListener
+         * sets the checkmark visible
+         */
+        editName.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                // Wenn editName nicht mehr ausgewählt ist, überprüfe den Text und zeige das Checkmark an, wenn nötig
+                val drawableEndVisibility = if (editName.text.isNullOrBlank()) View.INVISIBLE else View.VISIBLE
+                correctName = editName.text.isNotEmpty()
+                editName.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    0, 0, if (drawableEndVisibility == View.VISIBLE) R.drawable.checkmark else 0, 0
+                )
+            }
+        }
+        editEmail.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                // Wenn editName nicht mehr ausgewählt ist, überprüfe den Text und zeige das Checkmark an, wenn nötig
+                val drawableEndVisibility = if (editEmail.text.isNullOrBlank()) View.INVISIBLE else View.VISIBLE
+                correctEmail = editEmail.text.isNotEmpty()
+                editEmail.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    0, 0, if (drawableEndVisibility == View.VISIBLE) R.drawable.checkmark else 0, 0
+                )
+            }
+        }
+        editPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                // Wenn editName nicht mehr ausgewählt ist, überprüfe den Text und zeige das Checkmark an, wenn nötig
+                val drawableEndVisibility = if (editPassword.text.isNullOrBlank()) View.INVISIBLE else View.VISIBLE
+                correctPassword = editPassword.text.isNotEmpty()
+                editPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    0, 0, if (drawableEndVisibility == View.VISIBLE) R.drawable.checkmark else 0, 0
+                )
+            }
+        }
+
+        /**
+         * if touched outside of the editTexts
+         * set the editTexts not being focused anymore
+         * this triggers onFocusChange
+         */
+        view.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                // Überprüfe, ob der Klick außerhalb der EditTexts war
+                if (editName.isFocused || editPassword.isFocused || editEmail.isFocused) {
+                    val outRect = android.graphics.Rect()
+                    editName.getGlobalVisibleRect(outRect)
+                    editEmail.getGlobalVisibleRect(outRect)
+                    if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                        // Der Klick war außerhalb der EditTexts, entferne den Fokus
+                        editName.clearFocus()
+                        editEmail.clearFocus()
+                        editPassword.clearFocus()
+                    }
+                }
+            }
+            false
+        }
+        return view
     }
 }
